f422d90ea5bee8a40e8a612e0cb9d71859d8e7a2
MostafaSoleimani <88878976+MostafaSoleimani@users.noreply.github.com>
2022-11-18 17:04:47 +0330

    Feat: Add Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
-import FormDesigner from './core/container/FormDesigner';
+import {
+  BrowserRouter, Link, Route,
+  Routes
+} from "react-router-dom";
+import "./App.css";
+import FormDesigner from "./core/container/FormDesigner";
 
 function App() {
   return (
-    <div className="App">
-      <FormDesigner/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Link to="design">Go To Design</Link>}
+        ></Route>
+        <Route path="design" element={<FormDesigner />} />
+        <Route
+          path="engine"
+          element={<h1> Engine is not implemented yet</h1>}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
